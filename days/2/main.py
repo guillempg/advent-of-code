a3f3ffb88e799da09a65f34c927b96e08cf934ca
Guillem Plasencia <50019082+guillemplasencia@users.noreply.github.com>
2021-11-18 15:49:44 +0100

    Advent of code 2020, day 2 part 1

--- days/2/main.py
+++ days/2/main.py
@@ -1,44 +1,35 @@
-def pairs_with_summed_value(values, summed):
-    output = []
-    for v1 in values:
-        for v2 in values:
-            added = int(v1) + int(v2)
-            print("added:", added)
-            if (int(v1) != int(v2) and
-                    added == summed):
-                output.append(v1)
-                output.append(v2)
-    return output
-
-def trios_with_summed_value(values, summed):
-    output = []
-    for v1 in values:
-        for v2 in values:
-            for v3 in values:
-                added = int(v1) + int(v2) + int(v3)
-                print("added:", added)
-                if (int(v1) != int(v2) and
-                    int(v2) != int(v3) and
-                    int(v1) != int(v3) and
-                    added == summed):
-                    output.append(v1)
-                    output.append(v2)
-                    output.append(v3)
-    return output
+def isValidPassword(character, atLeast, noMoreThan, password):
+    #print("password:", password, "character:", character, "count:", password.count(character))
+    if (password.count(character) < int(atLeast) or
+            password.count(character) > int(noMoreThan)):
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
     with open('input') as file:
         lines = file.readlines()
         lines = [line.rstrip() for line in lines]
-    print("values:", lines)
-    pairs = pairs_with_summed_value(lines, 2020)
-    print("pairs:", pairs)
-    for previous, current in zip(pairs, pairs[1:]):
-        multiplied = int(previous)*int(current)
-        print("Multiplying: ", previous, " with ", current, " gives:", multiplied)
-    trios = trios_with_summed_value(lines, 2020)
-    print("trios:", trios)
-    for previous2, previous, current in zip(trios, trios[1:],trios[2:]):
-        multipliedTrio = int(previous2) * int(previous) * int(current)
-        print("Multiplying: ", previous2, " with ", previous, " with ", current, " gives:", multipliedTrio)
+    #print("values:", lines)
+    ## Example '9-10 m: mmmmnxmmmwm'
+    passwordToRule = {}
+    invalid = 0
+    print("There are ", len(lines), " passwords")
+    for count, line in enumerate(lines):
+        passwordToRule[count] = {}
+        line.rstrip()
+        line.lstrip()
+        line = line.replace(':', '')
+        #print("Line after remove colon:", line)
+        fields = line.split(' ')
+        times = fields[0].rstrip()
+        minTimes = times.split("-")[0]
+        maxTimes = times.split("-")[1]
+        character = fields[1].replace(' ', '').rstrip()
+        password = fields[2].replace(' ', '').rstrip()
+        #print("MinTimes:", minTimes, "|MaxTimes:", maxTimes, "|Character:", character, "|Password:", password)
 
+        if not isValidPassword(character, minTimes, maxTimes, password):
+            print("charCount:", password.count(character), "line:", line)
+            invalid = invalid+1
+            print("Total valid passwords:", len(lines)-invalid)
